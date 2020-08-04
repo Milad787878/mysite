05d33d8a384c8e0ad0141e12acc004d6bc021599
Milad787878 <mohammadjavad.alghosi78@gmail.com>
2020-08-04 23:56:18 +0430

    Updates

---
+++ precache-manifest.663b341c93fe481e700704593177a027.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35841f7d0510400f682598bdc5c96e39",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "c0759f75dff3bcb6af1b",
+    "url": "/mysite/static/css/2.a7128330.chunk.css"
+  },
+  {
+    "revision": "99a260a1726809014a3b",
+    "url": "/mysite/static/css/main.d57e275e.chunk.css"
+  },
+  {
+    "revision": "c0759f75dff3bcb6af1b",
+    "url": "/mysite/static/js/2.462cc6f9.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/mysite/static/js/2.462cc6f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99a260a1726809014a3b",
+    "url": "/mysite/static/js/main.3bdc61d1.chunk.js"
+  },
+  {
+    "revision": "0c3b7deba32f40e5d930",
+    "url": "/mysite/static/js/runtime-main.3cc60382.js"
+  },
+  {
+    "revision": "0b1de21f5bf50128a389e63a5f1c63e1",
+    "url": "/mysite/static/media/Frontcert.0b1de21f.png"
+  },
+  {
+    "revision": "22ad531053e99ceea127cb880ec3f960",
+    "url": "/mysite/static/media/Matlabcert.22ad5310.png"
+  },
+  {
+    "revision": "ef47691e988c1e5d155ee0953dee00e7",
+    "url": "/mysite/static/media/Pythoncert.ef47691e.png"
+  },
+  {
+    "revision": "2bee11a830bacc5ae9006df56b20c33a",
+    "url": "/mysite/static/media/React.2bee11a8.png"
+  },
+  {
+    "revision": "a5ef4d505ea01b5da11904fc14048a99",
+    "url": "/mysite/static/media/Reactcert.a5ef4d50.png"
+  },
+  {
+    "revision": "e9fcb24d2baf2342285f92b6b940762e",
+    "url": "/mysite/static/media/bootstrap.e9fcb24d.png"
+  },
+  {
+    "revision": "4cb3c10fe9c7fb52f4740e11619db531",
+    "url": "/mysite/static/media/daallogo.4cb3c10f.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mysite/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mysite/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mysite/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mysite/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mysite/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4a8c23476a7c20c5bee2a752a6f96e9e",
+    "url": "/mysite/static/media/instagram.4a8c2347.png"
+  },
+  {
+    "revision": "6a4c201cef84c174d0d93923f26b168c",
+    "url": "/mysite/static/media/javascript.6a4c201c.png"
+  },
+  {
+    "revision": "df0d19de804bfbbc9c7dd7fb96bc8305",
+    "url": "/mysite/static/media/jquery.df0d19de.png"
+  },
+  {
+    "revision": "5e7a971bb6b334ac935915c3d629e1b4",
+    "url": "/mysite/static/media/matlablogo.5e7a971b.png"
+  },
+  {
+    "revision": "21b580a48759940b7a73bff6be8ed530",
+    "url": "/mysite/static/media/me2.21b580a4.jpg"
+  },
+  {
+    "revision": "2283bbc90750cd6db5b6654c3c82b187",
+    "url": "/mysite/static/media/mizanlogo.2283bbc9.jpg"
+  },
+  {
+    "revision": "0c82ea0eafce9707da068a8af9b48ae8",
+    "url": "/mysite/static/media/sass.0c82ea0e.jpg"
+  },
+  {
+    "revision": "3a802502450ecff76b3bfe7919436a48",
+    "url": "/mysite/static/media/sharif1.3a802502.jpg"
+  },
+  {
+    "revision": "1d62f13b32a1a11924656945fb3ca377",
+    "url": "/mysite/static/media/shariflogo.1d62f13b.jpg"
+  },
+  {
+    "revision": "eb1fa44bd3e279f3f0b7c05df3bee9b9",
+    "url": "/mysite/static/media/telegram.eb1fa44b.png"
+  }
+]);
